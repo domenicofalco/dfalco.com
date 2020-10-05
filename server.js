a9f2d002fd6b33cfa5e8810e9bba4f1aa37358d7
dfalco <dfalco@ebay.com>
2020-10-05 16:35:08 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || "3001";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  console.log(`> Evironment: ${process.env.NODE_ENV}`);
+  const server = express();
+
+  server.get("*", handle);
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
