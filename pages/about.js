52d018b1ff64490b2bf76d08ea8d60ff5f3aea21
dfalco <dfalco@ebay.com>
2020-10-18 21:52:57 +0200

    add new page

--- pages/about.js
+++ pages/about.js
@@ -2,16 +2,16 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>dfalco.com - homepage</title>
+        <title>dfalco.com - about</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className={styles.title}>Next.js - homepage</h1>
-      <Link href="/about">go to about page (CSR)</Link>
+      <h1 className={styles.title}>Next.js - about page</h1>
+      <Link href="/">go to homepage (CSR)</Link>
     </div>
   );
 }
