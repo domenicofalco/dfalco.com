5205472656a73fee46b707c026cc1171833a7ecf
dfalco <dfalco@ebay.com>
2020-10-18 18:36:32 +0200

    changes

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className={styles.title}>Welcome to Next.js</h1>
+      <h1 className={styles.title}>Welcome to Next.js ciao</h1>
     </div>
   );
 }
